cd93f77f83024b02ee75ab21e3142adfb575e70f
dwainwright999 <49505567+dwainwright999@users.noreply.github.com>
2020-04-13 21:02:39 -0400

    Update FSAEDynoDataVis.py

--- FSAEDynoDataVis.py
+++ FSAEDynoDataVis.py
@@ -5,12 +5,13 @@ from matplotlib import style
 style.use('fivethirtyeight')
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(1, 1, 1)
 
 path = 'C:\\Users\\DanHamptonWainwright\\Western Michigan University\\RSO, Formula Racing - Formula SAE\\2020\\Testing\\Dyno\\.CSV dump\\dynodata'
 
+
 def animate(i):
-    graph_data = open(path,'r').read()
+    graph_data = open(path, 'r').read()
     lines = graph_data.split('\n')
     x = []
     y1 = []
@@ -25,6 +26,7 @@ def animate(i):
     ax1.clear()
     ax1.plot(x, y1)
     ax1.plot(x, y2)
+
 
 ani = animation.FuncAnimation(fig, animate, interval=250)
 plt.show()
