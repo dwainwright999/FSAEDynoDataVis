0a332dac4ebf8f73353e6d27f3777a7a5731a81e
dwainwright999 <49505567+dwainwright999@users.noreply.github.com>
2020-04-23 22:29:59 -0400

    Update FSAEDynoDataVis.py

--- FSAEDynoDataVis.py
+++ FSAEDynoDataVis.py
@@ -1,14 +1,21 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
+from multiprocessing import Process
+import serial
+import pandas as pd
+import numpy as np
+
+port  = 'COM6'
+path = 'C:\\Users\\DanHamptonWainwright\\Western Michigan University\\RSO, Formula Racing - Formula SAE\\2020\\Testing\\Dyno\\.CSV dump\\dynodata'
+
+
+ser = serial.Serial(port, 921600, )
 
 style.use('fivethirtyeight')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
-
-path = 'C:\\Users\\DanHamptonWainwright\\Western Michigan University\\RSO, Formula Racing - Formula SAE\\2020\\Testing\\Dyno\\.CSV dump\\dynodata'
-
 
 def animate(i):
     graph_data = open(path, 'r').read()
@@ -27,6 +34,14 @@ def animate(i):
     ax1.plot(x, y1)
     ax1.plot(x, y2)
 
+def getserial():
+    packet = ser.readline()
+    df = pd.DataFrame(packet)
+    df.to_csv(path)
+
+if __name__ == '__main__':
+    DAQ = Process(target = getserial)
+    DAQ.start()
 
 ani = animation.FuncAnimation(fig, animate, interval=250)
 plt.show()
