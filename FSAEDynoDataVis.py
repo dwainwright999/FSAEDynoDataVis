2f29e8545b4d05305bbf2cef8b589dc63d2e8d25
dwainwright999 <49505567+dwainwright999@users.noreply.github.com>
2020-04-27 22:08:06 -0400

    Update FSAEDynoDataVis.py

--- FSAEDynoDataVis.py
+++ FSAEDynoDataVis.py
@@ -1,16 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-from multiprocessing import Process
-import serial
-import pandas as pd
-import numpy as np
 
-port  = 'COM6'
+
 path = 'C:\\Users\\DanHamptonWainwright\\Western Michigan University\\RSO, Formula Racing - Formula SAE\\2020\\Testing\\Dyno\\.CSV dump\\dynodata'
-
-
-ser = serial.Serial(port, 921600)
 
 style.use('fivethirtyeight')
 
@@ -18,30 +11,28 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 
 def animate(i):
-    graph_data = open(path, 'r').read()
-    lines = graph_data.split('\n')
-    x = []
-    y1 = []
-    y2 = []
+    try:
+        graph_data = open(path, 'r').read()
+        lines = graph_data.split('\n')
+        t = []
+        rpm = []
+        tau = []
+        hp = []
+        
+        for line in lines:
+            if len(line) > 10:
+                a, b, c, d = line.split(',')
+                t.append(float(a))
+                rpm.append(float(b))
+                tau.append(float(c))
+                hp.append(float(d))
+        ax1.clear()
+        ax1.plot(t, rpm, linewidth = 1)
+        ax1.plot(t, tau, linewidth = 1)
+        ax1.plot(t, hp, linewidth = 1)
+    
+    except:
+        pass
 
-    for line in lines:
-        if len(line) > 1:
-            a, b, c = line.split(',')
-            x.append(float(a))
-            y1.append(float(b))
-            y2.append(float(c))
-    ax1.clear()
-    ax1.plot(x, y1)
-    ax1.plot(x, y2)
-
-def getserial():
-    packet = ser.readline()
-    df = pd.DataFrame(packet)
-    df.to_csv(path)
-
-if __name__ == '__main__':
-    DAQ = Process(target = getserial)
-    DAQ.start()
-
-ani = animation.FuncAnimation(fig, animate, interval=250)
+ani = animation.FuncAnimation(fig, animate)
 plt.show()
